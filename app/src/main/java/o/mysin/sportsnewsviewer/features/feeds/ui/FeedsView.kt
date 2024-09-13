5da384979baed086944d0aa42cf249ff35eaa1bd
Oleg Mysin <7nacu7@gmail.com>
2024-09-13 21:51:28 +0300

    - add loading indicator for paging feed list;

--- app/src/main/java/o/mysin/sportsnewsviewer/features/feeds/ui/FeedsView.kt
+++ app/src/main/java/o/mysin/sportsnewsviewer/features/feeds/ui/FeedsView.kt
@@ -1,5 +1,6 @@
 package o.mysin.sportsnewsviewer.features.feeds.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import o.mysin.sportsnewsviewer.R
@@ -20,6 +23,7 @@ import o.mysin.sportsnewsviewer.data.model.NewsItemUI
 import o.mysin.sportsnewsviewer.features.feeds.presentation.models.FeedsEvent
 import o.mysin.sportsnewsviewer.features.feeds.presentation.models.FeedsViewState
 import o.mysin.sportsnewsviewer.features.feeds.ui.views.FeedCard
+import o.mysin.sportsnewsviewer.ui.common.LoadingIndicator
 import o.mysin.sportsnewsviewer.ui.theme.SportsTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -56,6 +60,30 @@ internal fun FeedsView(
                     FeedCard(it) { feedId ->
                         eventHandler.invoke(FeedsEvent.FeedClicked(feedId))
                     }
+                }
+            }
+            item {
+                when (val state = data.loadState.refresh) {
+                    is LoadState.Error -> {
+                        Log.d(
+                            stringResource(R.string.tag_error_loading_feed_item),
+                            stringResource(R.string.error_feeds_view_loading_message, state)
+                        )
+                    }
+
+                    LoadState.Loading -> {
+                        LoadingIndicator()
+                    }
+
+                    else -> {}
+                }
+
+                when (val state = data.loadState.append) {
+                    LoadState.Loading -> {
+                        LoadingIndicator()
+                    }
+
+                    else -> {}
                 }
             }
         }
